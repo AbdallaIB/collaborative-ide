b0748d3c547ae037df09b0b619e00cdd0f4495ba
abdallaIb <mrlesway5@gmail.com>
2022-11-10 13:53:34 +0000

    feat(client): add auth & project yup schemas + page titles

---
+++ client/src/utils/helpers.ts
@@ -0,0 +1,6 @@
+export const PageTitles = {
+  '/': 'Home | Collaborative Ide',
+  '/code': 'Code | Collaborative Ide',
+  '/login': 'Login | Collaborative Ide',
+  '/signup': 'Sign Up | Collaborative Ide',
+};
---
+++ client/src/utils/schemas.ts
@@ -0,0 +1,43 @@
+import { object, ref, string } from 'yup';
+
+export const signUpSchema = object().shape({
+  email: string().email('Email is not valid').required('Email is required'),
+  username: string()
+    .min(5, 'Username must be at least 5 characters')
+    .max(20, 'Username must be at most 20 characters')
+    .required('Username is required'),
+  password: string()
+    .min(8, 'Password must be at least 8 characters')
+    .max(32, 'Password must be at most 32 characters')
+    .required('Password is required'),
+  confirmPassword: string()
+    .required('Confirm password is required')
+    .oneOf([ref('password')], 'Passwords do not match.'),
+});
+
+export type SignUpSchemaType = typeof signUpSchema;
+
+export const loginSchema = object().shape({
+  username: string()
+    .min(5, 'Username must be at least 5 characters')
+    .max(20, 'Username must be at most 20 characters')
+    .required('Username is required'),
+  password: string()
+    .min(8, 'Password must be at least 8 characters')
+    .max(32, 'Password must be at most 32 characters')
+    .required('Password is required'),
+});
+export type LoginSchemaType = typeof loginSchema;
+
+export const createProjectSchema = object().shape({
+  title: string().required('Project title is required'),
+});
+
+export type CreateProjectSchemaType = typeof createProjectSchema;
+
+export const joinProjectSchema = object().shape({
+  sessionId: string().required('Session Id is required'),
+  username: string().required('Username is required'),
+});
+
+export type JoinProjectSchemaType = typeof joinProjectSchema;
