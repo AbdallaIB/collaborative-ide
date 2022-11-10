8fca6d3928cd0e3497e2e33ffd2c337ba7f92fc2
abdallaIb <mrlesway5@gmail.com>
2022-11-10 14:13:11 +0000

    feat(client): add cancellable & darkmode feature to modal

--- client/src/components/modal/index.tsx
+++ client/src/components/modal/index.tsx
@@ -1,7 +1,8 @@
-import './index.css';
 import Modal from 'react-modal';
 import { ReactNode } from 'react';
 import Button from '@components/shared/button';
+import './index.css';
+import useDarkModeStore from '@lib/stores/darkMode';
 
 export interface Props {
   title: string;
@@ -10,6 +11,7 @@ export interface Props {
   confirmText: string;
   styles: { content: { height: string; width: string } };
   hasFooter: boolean;
+  isCancellable?: boolean;
   cancel: () => void;
   confirm: () => void;
 }
@@ -25,33 +27,47 @@ const customStyles = {
   },
 };
 
-const Modals = ({ title, isOpen, children, cancel, confirm, confirmText, styles, hasFooter = true }: Props) => {
+const Modals = ({
+  title,
+  isOpen,
+  children,
+  cancel,
+  confirm,
+  confirmText,
+  styles,
+  hasFooter = true,
+  isCancellable = true,
+}: Props) => {
+  const { darkMode } = useDarkModeStore();
   return (
     <Modal
       style={{ content: { ...customStyles.content, ...styles.content, overflow: 'hidden' } }}
       ariaHideApp={false}
       contentLabel={title}
       isOpen={isOpen}
-      //   toggle={() => cancel()}
     >
-      <header className="header">
-        <h2 className="font-medium"> {title} </h2>
-        <button className="h-full text-2xl" onClick={() => cancel()}>
-          <i className="bx bx-x"></i>
-        </button>
-      </header>
-      <main
-        className={'flex flex-col items-center justify-center w-full mt-4'}
-        style={{ borderBottom: hasFooter ? '1px solid #dddddd' : '', height: '85%' }}
-      >
-        {children}
-      </main>
-      {hasFooter && (
-        <footer className="footer">
-          <Button text="Cancel" onClick={() => cancel()} color="gray"></Button>
-          <Button text={confirmText} onClick={() => confirm()} isPrimary></Button>
-        </footer>
-      )}
+      <section className={'modal ' + (darkMode ? 'dark' : '')}>
+        <header className="header">
+          <h2 className="font-medium text-main"> {title} </h2>
+          {isCancellable && (
+            <button className="h-full text-2xl" onClick={cancel}>
+              <i className="bx bx-x text-main_dark"></i>
+            </button>
+          )}
+        </header>
+        <main
+          className={'flex flex-col items-center justify-center w-full mt-4'}
+          style={{ borderBottom: hasFooter ? '1px solid #dddddd' : '', height: '85%' }}
+        >
+          {children}
+        </main>
+        {hasFooter && (
+          <footer className="footer">
+            {isCancellable && <Button text="Cancel" onClick={cancel} color="gray"></Button>}
+            <Button text={confirmText} onClick={confirm} isPrimary></Button>
+          </footer>
+        )}
+      </section>
     </Modal>
   );
 };
